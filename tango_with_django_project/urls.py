38363f7d353063cbc763a416571b5a29b29e570a
Elliott Nelson <elliottdavidnelson745@gmail.com>
2026-02-06 15:03:19 +0000

    Chapter 4 commit: fixed templates, static files, views

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -17,9 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from rango import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('', views.index, name='index'),       # home page
     path('rango/', include('rango.urls')),     # /rango/ goes to the Rango app
     path('admin/', admin.site.urls),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
