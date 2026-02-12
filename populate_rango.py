1179e9e890286d198dfd521fd52375118dd6d7bf
Elliott Nelson <elliottdavidnelson745@gmail.com>
2026-02-12 14:05:27 +0000

    Chapter 6

---
+++ populate_rango.py
@@ -0,0 +1,109 @@
+import os
+
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE',
+    'tango_with_django_project.settings'
+)
+
+import django
+django.setup()
+
+from rango.models import Category, Page
+
+
+def populate():
+    python_pages = [
+        {
+            'title': 'Official Python Tutorial',
+            'url': 'http://docs.python.org/3/tutorial/',
+            'views' : 64
+        },
+        {
+            'title': 'How to Think like a Computer Scientist',
+            'url': 'http://www.greenteapress.com/thinkpython/' , 
+            'views' : 64
+        },
+        {
+            'title': 'Learn Python in 10 Minutes',
+            'url': 'http://www.korokithakis.net/tutorials/python/',
+            'views' : 64
+        }
+    ]
+
+    django_pages = [
+        {
+            'title': 'Official Django Tutorial',
+            'url': 'https://docs.djangoproject.com/en/2.1/intro/tutorial01/',
+            'views' : 64
+        },
+        {
+            'title': 'Django Rocks',
+            'url': 'http://www.djangorocks.com/',
+            'views' : 64
+        },
+        {
+            'title': 'How to Tango with Django',
+            'url': 'http://www.tangowithdjango.com/',
+            'views' : 64
+        }
+    ]
+
+    other_pages = [
+        {
+            'title': 'Bottle',
+            'url': 'http://bottlepy.org/docs/dev/',
+            'views' : 64
+        },
+        {
+            'title': 'Flask',
+            'url': 'http://flask.pocoo.org',
+            'views' : 64
+        }
+    ]
+
+    cats = {
+        'Python': {'pages': python_pages},
+        'Django': {'pages': django_pages},
+        'Other Frameworks': {'pages': other_pages}
+    }
+
+    for cat, cat_data in cats.items():
+    # assign views and likes based on category
+        if cat == 'Python':
+            c = add_cat(cat, views=128, likes=64)
+        elif cat == 'Django':
+            c = add_cat(cat, views=64, likes=32)
+        elif cat == 'Other Frameworks':
+            c = add_cat(cat, views=32, likes=16)
+                
+        for p in cat_data['pages']:
+            add_page(c, p['title'], p['url'], p['views'])
+
+    for c in Category.objects.all():
+        for p in Page.objects.filter(category=c):
+            print(f'- {c}: {p}')
+
+
+def add_page(cat, title, url, views):
+    p = Page.objects.get_or_create(
+        category=cat,
+        title=title
+    )[0]
+
+    p.url = url
+    p.views = views
+    p.save()
+
+    return p
+
+
+def add_cat(name , views , likes=0):
+    c = Category.objects.get_or_create(name=name)[0]
+    c.views = views
+    c.likes = likes
+    c.save()
+    return c
+
+if __name__ == '__main__':
+    print('Starting Rango population script...')
+    populate()
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -24,5 +24,8 @@ urlpatterns = [
     path('', views.index, name='index'),       # home page
     path('rango/', include('rango.urls')),     # /rango/ goes to the Rango app
     path('admin/', admin.site.urls),
+    path('about/', views.about, name='about'),
+    path('category/<slug:category_name_slug>/',
+        views.show_category, name='show_category'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
